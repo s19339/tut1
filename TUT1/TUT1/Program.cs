f3b8981418aa3c3b507dcea69ea25ad404b036c7
Kunal Kashyap <s19339@pjwstk.edu.pl>
2020-03-05 16:58:16 +0100

    initial␣commit

---
+++ TUT1/TUT1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Tutorial1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+
+            var websiteUrl = args[0];
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(websiteUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                var htmlcontent = await response.Content.ReadAsStringAsync();
+
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                var matches = regex.Matches(htmlcontent);
+
+                foreach (var emailAddress in matches)
+                {
+                    Console.WriteLine(emailAddress.ToString());
+                }
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
